d89474ca8e5cb3836f5e85ba5cf3edfaf2cbe9ad
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-07-22 11:20:29 -0500

    [codestyle]: fix documentation and indent of the code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,11 +4,13 @@
 /**
  * binary_tree_node - insert a binary tree node
  * @parent: The parent(branch) of the leaf to insert
- * @tree: Pointer to the root node of the tree to print
+ * @value: Ordinal value to insert in binary tree
+ * Return: Pointer to the node inserted
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)

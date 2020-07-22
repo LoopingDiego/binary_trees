1ad12810590369d43f4297b6575d243d39a8d1a1
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-07-22 12:11:07 -0500

    [fix]: node wont be create if parent is not available

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,10 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = binary_tree_node(parent, value);
-	if (!new_node || !parent)
+	if (parent)
+		new_node = binary_tree_node(parent, value);
+	else
 		return (NULL);
-	else if (parent->left)
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;

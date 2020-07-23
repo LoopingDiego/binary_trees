5e060dbef60b42fbe2de6fd8208d368674212800
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-07-23 10:48:15 -0500

    [not working]: save binary tree fulness checker :)
    This is a checkpoint, code still not works properly

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_is_full - returns eval of tree fullness
+ * @tree: pointer to root
+ * Return: 0 if not full, 1 on fullness
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int siz_left = 0;
+	int siz_right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		siz_left = binary_tree_is_full(tree->left);
+		siz_left += 1;
+		siz_right = binary_tree_is_full(tree->right);
+		siz_right += 1;
+	}
+	if (!tree->parent)
+		return (siz_left == siz_right);
+	if (siz_left > siz_right)
+		return (siz_left);
+	else
+		return (siz_right);
+}
